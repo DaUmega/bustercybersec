b0185d00b90056e721c93a1dd8f0b4268b5f4977
DaUmega <daumega@gmail.com>
2024-10-27 22:50:26 -0600

    ports script and crontab instructions update

--- ports.sh
+++ ports.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-/usr/sbin/iptables -I INPUT -p tcp --dport 80 -j ACCEPT
-/usr/sbin/iptables -I INPUT -p tcp --dport 443 -j ACCEPT
-/usr/sbin/iptables -I INPUT -p udp --dport 53 -j ACCEPT
+iptables_path=$(command -v iptables)
+
+$iptables_path -I INPUT -p tcp --dport 80 -j ACCEPT
+$iptables_path -I INPUT -p tcp --dport 443 -j ACCEPT
+$iptables_path -I INPUT -p udp --dport 53 -j ACCEPT
